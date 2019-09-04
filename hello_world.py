100981b7e4982abda94d991c42f23a549c6bc96a
alchen <alchen@vmware.com>
2019-09-04 15:56:41 -0700

    add counter to script

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,10 @@ print('hello world outside function')
 def run_hello_world():
     print('Hello World')
     print('first change being made to the build')
+    count = 0
+    for i in range(10):
+        print(count)
+        count += 1
 
 
 def first_change():
