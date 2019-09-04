d278b1486f478bc2eb2bd5aeb917a186e8d661b8
alchen <alchen@vmware.com>
2019-09-04 15:36:16 -0700

    first Jenkin/github integration commit

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# class HelloWorld:
+print('hello world outside function')
+
+def run_hello_world():
+    print('Hello World')
+    print('first change being made to the build')
+
+
+def first_change():
+    print('first change being made to the build')
