495f33b9b240fc2cbca4e2002a23a37385ab281f
alchen <alchen@vmware.com>
2019-09-05 14:56:58 -0700

    chaning parameter input to system arg.

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,21 @@
+import os
+import sys
 # class HelloWorld:
 print('hello world outside function')
+
+
+platform = sys.argv[1]
 
 def run_hello_world(platform):
     print('Hello World')
     print('first change being made to the build')
     count = 0
     for i in range(10):
-        print(count, 'I am using ', platform, "OS")
+        print(str(count), 'I am using ', platform, "OS")
         count += 1
 
 
 def first_change():
     print('first change being made to the build')
+
+run_hello_world(platform)
