2fdad0a385bf288b308f7446231391042ec80b6b
alchen <alchen@vmware.com>
2019-09-05 14:37:03 -0700

    adding parameter

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,12 @@
 # class HelloWorld:
 print('hello world outside function')
 
-def run_hello_world():
+def run_hello_world(platform):
     print('Hello World')
     print('first change being made to the build')
     count = 0
     for i in range(10):
-        print(count)
+        print(count, 'I am using ', platform, "OS")
         count += 1
 
 
